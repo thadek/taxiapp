1acd72bf457e81e8a98d3e7f81589d3bfe920dff
Fausto <faustobilo47@gmail.com>
2024-09-18 17:40:15 -0300

    landing page en proceso

--- frontend/taxiapp/app/page.tsx
+++ frontend/taxiapp/app/page.tsx
@@ -1,17 +1,27 @@
-"use client";
 
 import Header from "@components/Header";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
       <div>
-            <Header />
+          <Header />
           <div className="flex flex-col gap-4">
               <div className="flex flex-col items-center justify-center h-screen">
+
                   <h1>Bienvenido a la taxiApp</h1>
                   <p>La mejor app de taxis del mercado!</p>
-
+                  <Image
+                      src="/imgTaxiTrompa.png"
+                      alt="trompa de taxi"
+                      width={500}
+                      height={500}
+                      className=""
+                  />
+                  <section className="flex bg-basil h-screen w-9/12 rounded-full items-center justify-center">
+                      <p>Somos tu comodidad</p>
+                  </section>
               </div>
           </div>
       </div>
--- frontend/taxiapp/components/Header.tsx
+++ frontend/taxiapp/components/Header.tsx
@@ -2,9 +2,9 @@
 
 export default function Header(){
     return (
-        <header className="flex items-center justify-between bg-forest text-white p-4 rounded-b-xl">
+        <header className="flex items-center justify-between bg-forest text-white p-4 rounded-b-xl pl-24">
             <a href="#" className="text-white">TaxiApp</a>
-            <div className="flex gap-4">
+            <div className="flex gap-4 pr-24">
                 <a href="/login" className="text-white">Iniciar sesion</a>
                 <a href="/register" className="text-white">Registrarse</a>
             </div>
